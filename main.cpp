1d521036f2f3ca4ff924ee01fb2c110fa351978a
bhoneha <bhonehtetaung28655@gmail.com>
2024-09-21 22:25:20 +0800

    finished main program

--- main.cpp
+++ main.cpp
@@ -1,34 +1,68 @@
 #include <iostream>
 #include "Parser/Parser.hpp"
 #include "Code/Code.hpp"
+#include "translations.hpp"
+#include "SymbolTable/SymbolTable.hpp"
 
 int main() {
 	std::ofstream file("out.hack");
+	// parser to contruct symbol table
 	Parser p("test.asm");
+	// parser for translation
+	Parser i("test.asm");
+
 	Code c;
+	translations t;
+	SymbolTable table;
+
+	unsigned int rom_addr = 0;
+	unsigned int ram_addr = 16;
+
 	while (p.hasMoreLines()) {
 		p.advance();
-		if (p.instructionType() == inst_type::A_INST) {
-			file << "0" << c.A_const(p.symbol()) << '\n';
+
+		if (p.instructionType() == inst_type::LABEL) {
+			std::string sym = p.symbol();
+			if (!table.contains(sym)) {
+				table.addEntry(sym, rom_addr);
+			}
 		}
-		else if (p.instructionType() == inst_type::C_INST) {
-			file << "111" << c.comp(p.comp()) << c.dest(p.dest()) << c.jump(p.jump()) << '\n';
+		if (p.instructionType() == inst_type::A_INST || p.instructionType() == inst_type::C_INST) rom_addr++;
+	}
+	
+
+	while (i.hasMoreLines()) {
+		i.advance();
+		
+		if (i.instructionType() == inst_type::A_INST) {
+			std::string sym = i.symbol();
+			bool digit = false;
+			for (char c : sym) {
+				digit = std::isdigit(c);
+				if (digit == false) {
+					break;
+				}
+			}
+
+			if (!digit) {
+				if (!table.contains(sym)) {
+					table.addEntry(sym, ram_addr);
+					ram_addr++;
+				}
+			file << "0" << t.dec_to_bin(table.getAddress(sym)) << '\n';
+
+			}
+			else if(digit){
+				file << "0" << t.dec_to_bin(sym) << '\n';
+			}
 		}
-		else {
-			continue;
+
+		if (i.instructionType() == inst_type::C_INST) {
+			file << "111" << c.comp(i.comp()) << c.dest(i.dest()) << c.jump(i.jump()) << '\n';
 		}
 	}
 
-	// Debug code
-	/*p.advance();
-	std::cout << p.curr_inst << "  =>  " << int(p.instructionType()) << '\n';*/
 
-	//std::cout << "Dest : " << p.dest() << '\n';
-	//std::cout << "Comp : " << p.comp() << '\n';
-	//std::cout << "Jump : " << p.jump() << '\n';
-	//std::cout << "addr : " << p.symbol() << '\n';
-	//// std::cout << "@" << c.A_const(p.symbol()) << '\n';
-	//std::cout << "111" << " | " << c.comp(p.comp()) << " | " << c.dest(p.dest()) << " | " << c.jump(p.jump()) << '\n';
-	
+	std::cout << table.getAddress("ponggame.0");
 	return 0;
 }
