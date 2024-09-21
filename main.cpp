e4d8cf98ec8c9b39516f052c7bd7c0400dd7e56c
bhoneha <bhonehtetaung28655@gmail.com>
2024-09-21 15:18:20 +0800

    testing program

--- main.cpp
+++ main.cpp
@@ -1,12 +1,34 @@
 #include <iostream>
-#include "Parser/parser.hpp"
+#include "Parser/Parser.hpp"
+#include "Code/Code.hpp"
 
 int main() {
-    // testing
+	std::ofstream file("out.hack");
 	Parser p("test.asm");
+	Code c;
 	while (p.hasMoreLines()) {
 		p.advance();
-		std::cout << p.curr_inst << " -> " << int(p.instructionType()) << '\n';
+		if (p.instructionType() == inst_type::A_INST) {
+			file << "0" << c.A_const(p.symbol()) << '\n';
+		}
+		else if (p.instructionType() == inst_type::C_INST) {
+			file << "111" << c.comp(p.comp()) << c.dest(p.dest()) << c.jump(p.jump()) << '\n';
+		}
+		else {
+			continue;
+		}
 	}
+
+	// Debug code
+	/*p.advance();
+	std::cout << p.curr_inst << "  =>  " << int(p.instructionType()) << '\n';*/
+
+	//std::cout << "Dest : " << p.dest() << '\n';
+	//std::cout << "Comp : " << p.comp() << '\n';
+	//std::cout << "Jump : " << p.jump() << '\n';
+	//std::cout << "addr : " << p.symbol() << '\n';
+	//// std::cout << "@" << c.A_const(p.symbol()) << '\n';
+	//std::cout << "111" << " | " << c.comp(p.comp()) << " | " << c.dest(p.dest()) << " | " << c.jump(p.jump()) << '\n';
+	
 	return 0;
 }
