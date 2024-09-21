e27854129d03562807a2bec4d9a730acc52225c3
bhoneha <bhonehtetaung28655@gmail.com>
2024-09-21 15:16:59 +0800

    finished translation mnemonics to binary

--- Code/Code.cpp
+++ Code/Code.cpp
@@ -1,1 +1,42 @@
+#include "Code.hpp"
+
+std::string Code::dest(std::string dest)
+{
+	std::sort(dest.begin(), dest.end());
+	if (!dest.empty()) {
+		if (Code::pairs.dest.find(dest) == Code::pairs.dest.end()) {
+			return "Instruction not found";
+		}
+		return Code::pairs.dest[dest];
+	}
+	return Code::pairs.dest[""];
+}
+
+std::string Code::comp(std::string comp)
+{
+	if (!comp.empty()) {
+		if (comp.find("M") == std::string::npos) return Code::pairs.comp_A[comp];
+		else return Code::pairs.comp_M[comp];
+	}
+	return "Instruction not found";
+}
+
+std::string Code::jump(std::string jump)
+{
+	if (!jump.empty()) {
+		if (Code::pairs.jump.find(jump) == Code::pairs.jump.end()) {
+			return "Instruction not found";
+		}
+		return Code::pairs.jump[jump];
+	}
+	return Code::pairs.jump[""];
+}
+
+std::string Code::A_const(std::string decimal)
+{
+	unsigned int dec = std::stoi(decimal);
+	std::bitset<15> bs{ dec };
+
+	return bs.to_string();
+}
 
--- Code/Code.hpp
+++ Code/Code.hpp
@@ -1,1 +1,20 @@
+#pragma once
+
+#include "../translations.hpp"
+#include <string>
+#include <algorithm>
+#include <bitset>
+
+class Code
+{
+private:
+	translations pairs;
+public:
+	// translates mnemonics to binary
+	std::string dest(std::string);
+	std::string comp(std::string);
+	std::string jump(std::string);
+
+	std::string A_const(std::string); // translates the const of @xxx into binary
+};
 
